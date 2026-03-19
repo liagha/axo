8602bdd4ad49e00a9d973ad0bc5798719adf85ce
ali <itsaliaghaahmadi@gmail.com>
2026-03-19 23:27:58 +0330

    adding exact matching to resolver

--- src/resolver/scope.rs
+++ src/resolver/scope.rs
@@ -1,12 +1,14 @@
 use {
     super::{
         assessor::{Affinity, Aligner},
-        Resolver, ErrorKind, ResolveError,
+        ErrorKind, ResolveError, Resolver,
     },
     crate::{
         data::Identity,
         internal::{hash::Map, hash::Set},
-        parser::{Element, Symbol},
+        parser::{Element, ElementKind, Symbol, SymbolKind},
+        scanner::{Token, TokenKind},
+        resolver::Resolvable,
     },
     matchete::{Assessor, Scheme},
 };
@@ -78,6 +80,85 @@ impl Scope {
         scopes.get(&parent)?.find(target, scopes)
     }
 
+    pub fn fits<'a>(query: &Element<'a>, candidate: &Symbol<'a>) -> bool {
+        match (&query.kind, &candidate.kind) {
+            (
+                ElementKind::Literal(Token {
+                                         kind: TokenKind::Identifier(_),
+                                         ..
+                                     }),
+                _,
+            ) => true,
+            (ElementKind::Invoke(invoke), SymbolKind::Function(function)) => {
+                invoke.members.len() == function.members.len()
+            }
+            (ElementKind::Construct(construct), SymbolKind::Structure(structure)) => {
+                let candidates = structure
+                    .members
+                    .iter()
+                    .filter(|member| member.is_instance())
+                    .filter_map(|member| member.target())
+                    .collect::<Vec<_>>();
+
+                let members = construct
+                    .members
+                    .iter()
+                    .filter_map(|member| match &member.kind {
+                        ElementKind::Binary(binary) => binary.left.target(),
+                        _ => member.target(),
+                    })
+                    .collect::<Vec<_>>();
+
+                candidates == members
+            }
+            (ElementKind::Construct(construct), SymbolKind::Union(union)) => {
+                let candidates = union
+                    .members
+                    .iter()
+                    .filter(|member| member.is_instance())
+                    .filter_map(|member| member.target())
+                    .collect::<Vec<_>>();
+
+                let members = construct
+                    .members
+                    .iter()
+                    .filter_map(|member| match &member.kind {
+                        ElementKind::Binary(binary) => binary.left.target(),
+                        _ => member.target(),
+                    })
+                    .collect::<Vec<_>>();
+
+                members.len() == 1 && candidates.contains(&members[0])
+            }
+            _ => false,
+        }
+    }
+
+    pub fn exact<'a>(
+        &self,
+        target: &Element<'a>,
+        scopes: &Map<Identity, Scope>,
+        registry: &Map<Identity, Symbol<'a>>,
+    ) -> Option<Symbol<'a>> {
+        let query = target.target()?;
+        let mut current = Some(self);
+
+        while let Some(scope) = current {
+            for identity in &scope.symbols {
+                if let Some(symbol) = registry.get(identity) {
+                    if let Some(candidate) = symbol.target() {
+                        if query == candidate && Self::fits(target, symbol) {
+                            return Some(symbol.clone());
+                        }
+                    }
+                }
+            }
+            current = scope.parent.and_then(|id| scopes.get(&id));
+        }
+
+        None
+    }
+
     pub fn lookup<'a>(
         &self,
         target: &Element<'a>,
@@ -85,6 +166,10 @@ impl Scope {
         registry: &Map<Identity, Symbol<'a>>,
     ) -> Result<Symbol<'a>, Vec<ResolveError<'a>>> {
         if let Some(symbol) = Resolver::builtin(target) {
+            return Ok(symbol);
+        }
+
+        if let Some(symbol) = self.exact(target, scopes, registry) {
             return Ok(symbol);
         }
 
